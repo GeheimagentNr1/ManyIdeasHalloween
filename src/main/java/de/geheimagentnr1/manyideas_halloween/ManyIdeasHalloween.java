0ea204c5c22316bc528b232b129d9f4d60e55e9b
GeheimagentNr1 <geheimelache@hotmail.com>
2022-09-30 17:00:32 +0200

    Init mod

---
+++ src/main/java/de/geheimagentnr1/manyideas_halloween/ManyIdeasHalloween.java
@@ -0,0 +1,12 @@
+package de.geheimagentnr1.manyideas_doors;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+
+@Mod( ManyIdeasHalloween.MODID )
+public class ManyIdeasHalloween {
+	
+	
+	public static final String MODID = "manyideas_halloween";
+}
